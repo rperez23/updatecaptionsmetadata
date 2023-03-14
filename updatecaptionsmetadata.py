40ca00de81be4828db034421591ec9f20a2fd349
rperez23 <ronnie.lperez@gmail.com>
2023-03-14 15:35:41 -0700

    v004

--- updatecaptionsmetadata.py
+++ updatecaptionsmetadata.py
@@ -4,9 +4,16 @@ import re
 import sys
 import os
 import openpyxl
+import warnings
+
+warnings.filterwarnings('ignore', category=DeprecationWarning)
+
 
 sheet = '1. Master Metadata'
 hns   = []
+
+START_ROW = 4
+START_COL = 2
 
 #get house numbers from the command line and return the list
 def get_HouseNumbers():
@@ -33,6 +40,26 @@ def get_input(question):
 	data = input(question)
 	return data
 
+def getColNumNum(xlf,sheet,colname):
+
+
+	stop = True
+
+	for c in range(START_COL,100):
+
+		txt = str(ws.cell(row=START_ROW,column=c).value)
+		if txt == colname:
+			return c
+
+	print('\n   ~~~Could not find Column:', colname, '~~~')
+
+	wb.save(xlf)
+	wb.close()
+
+	sys.exit(1)
+
+	return c
+
 
 #get info from the user 
 vidpth = get_input('Give me your video s3 path: ')
@@ -40,16 +67,31 @@ cappth = get_input('Give me your captn s3 path: ')
 xlf    = get_input('Give me your xl file name : ')
 hns    = get_HouseNumbers()
 
-#open the Metadata sheet for read/write
+#open the Metadata xlf for read/write
 try:
 	wb = openpyxl.load_workbook(xlf)
 except:
 	print('\n','   ~~~Cannot open Metadata Sheet~~~\n')
 	sys.exit(1)
 
+#open the '1. Master Metadata' sheet for read/write
+try:
+	ws = wb[sheet]
+except:
+	print('\n','   ~~~Cannot open Metadata Sheet~~~\n')
+	sys.exit(1)
+
+#Supplier.OriginalName
+#Fremantle.HouseNumber
+#TWK.AncillaryName
+#getColNumNum(xlf,sheet,colname):
+
+movcol = getColNumNum(xlf,ws,'Supplier.OriginalName')
+hncol  = getColNumNum(xlf,ws,'Fremantle.HouseNumber')
+capcol = getColNumNum(xlf,ws,'TWK.AncillaryName')
+
+print(movcol,':',capcol,':',hncol)
 wb.save(xlf)
 wb.close()
-
-
 
 
