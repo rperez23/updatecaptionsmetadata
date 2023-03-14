5531af210c5e3f5cfc06d94f57753b809878b5c3
rperez23 <ronnie.lperez@gmail.com>
2023-03-14 09:44:03 -0700

    v002

--- updatecaptionsmetadata.py
+++ updatecaptionsmetadata.py
@@ -1,7 +1,30 @@
 #! /usr/bin/python3
 
+import re
+import sys
 import os
 import openpyxl
+
+sheet = '1. Master Metadata'
+hns   = []
+
+#get house numbers from the command line and return the list
+def get_HouseNumbers():
+
+	hnList = []
+
+	for n in range(1,len(sys.argv)):
+
+		capf = sys.argv[n]
+
+		if os.path.isfile(capf):
+			strList = capf.split('.')
+			hn  = strList[0]
+			m   = re.match("^BUZ_[A-Z0-9]+",hn)
+			if m:
+				hnList.append(hn)
+
+	return hnList
 
 #get inputfrom the user and return it
 def get_input(question):
@@ -15,7 +38,10 @@ def get_input(question):
 vidpth = get_input('Give me your video s3 path: ')
 cappth = get_input('Give me your captn s3 path: ')
 xlf    = get_input('Give me your xl file name : ')
+hns    = get_HouseNumbers()
 
-#print('results:', '\n', vidpth, '\n', cappth, '\n', xlf)
+
+
+
 
 
